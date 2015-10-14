f2f9b88af80c88121139eae728598771f93810cc
Tony Tiratay <tony.tiratay@gmail.com>
2015-10-14 20:01:04 +0200

    Add angular package and first tempalte

--- test-angular.js
+++ test-angular.js
@@ -1,23 +1,16 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
-}
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+ 
+  // This code only runs on the client
+  angular.module('simple-todos',['angular-meteor']);
+ 
+  angular.module('simple-todos').controller('TodosListCtrl', ['$scope',
+    function ($scope) {
+ 
+      $scope.tasks = [
+        { text: 'This is task 1' },
+        { text: 'This is task 2' },
+        { text: 'This is task 3' }
+      ];
+ 
+  }]);
 }
